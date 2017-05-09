6dadce374f9e955c4fea77c740d99db4f9629fc7
williamroscoe <mailreg+githubbifter@posteo.net>
2017-05-09 11:25:35 +0200

    Initial Commit

---
+++ Totp Token Manager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Totp Token Manager
+//
+//  Created by William Roscoe on 2017/05/09.
+//  Copyright © 2017 William Roscoe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
